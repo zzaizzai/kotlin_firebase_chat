de24d9f3ff57458b33eebafdbf5e59bd98e3e625
junsai window <junsai0530@gmail.com>
2022-07-09 15:47:53 +0900

    firebase setting

--- app/src/main/java/com/junsai/LoginActivity.kt
+++ app/src/main/java/com/junsai/LoginActivity.kt
@@ -2,11 +2,28 @@ package com.junsai
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val email:EditText = findViewById(R.id.login_et_email)
+        val password:EditText = findViewById(R.id.login_et_password)
+        val btnLogin:Button = findViewById(R.id.login_btn_login)
+        val backToRegistration:TextView = findViewById(R.id.login_tv_backtoregistration)
+
+        btnLogin.setOnClickListener {
+            Log.d("Login", "email: ${email.text.toString()}, password:${password.text.toString()}")
+        }
+
+        backToRegistration.setOnClickListener {
+            finish()
+        }
 
     }
 }
