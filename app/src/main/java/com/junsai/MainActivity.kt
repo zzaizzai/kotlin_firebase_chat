fde71fbc6e6ec2b14d2c54f20d1da3176a96cc84
junsai window <junsai0530@gmail.com>
2022-07-09 14:24:17 +0900

    Initial commit

---
+++ app/src/main/java/com/junsai/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.junsai
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val email:EditText = findViewById(R.id.register_edittext_email)
+        val password:EditText = findViewById(R.id.register_edittext_password)
+        val register:Button = findViewById(R.id.register_btn_register)
+        var alredy_text:TextView = findViewById(R.id.register_tv_already)
+
+
+
+        register.setOnClickListener {
+            Log.d("MainActivity", "Email is :" + email.text.toString())
+            Log.d("MainActivity", "Password:" + password.text.toString())
+
+        }
+
+        alredy_text.setOnClickListener {
+            Log.d("MainActivity", "Try to show login activity")
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+    }
+}
